8cd7feb2d6639e3cac467504f10120989ffaefb3
narendarm <38958885+narendarm@users.noreply.github.com>
2018-06-27 10:47:44 -0400

    Update prd_ss_I.jsx

--- Linux/prd_ss_I.jsx
+++ Linux/prd_ss_I.jsx
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <JSX>
         <CELL Name="SBCSSPROD"></CELL>
-        <J2C Provider="WebsphereMQ" Name="LSOSS_MechID" userId="m93869"></J2C>
+        <J2C Provider="WebsphereMQ" Name="LSOSS_MechID" userId="mechid93869"></J2C>
         <J2C Provider="TIBCO" Name="SS_ATLAS" userId="sbcss"></J2C>
 <POOL agedTO="0" connTO="180" maxConn="10" minConn="1" pPol="EntirePool" reapTime="180" unuTO="1800" mapConfAlias="DefaultPrincipalMapping"></POOL>    
 <AUTH Name="LSOSS_MechID"></AUTH>
